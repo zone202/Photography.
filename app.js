869342e9b7e3d2b45ae169ef107c3b08cb6f161b
Daniel Wolanski <loveallunknown@gmail.com>
2023-02-23 18:23:10 -0500

    seventh deploy

--- app.js
+++ app.js
@@ -87,9 +87,9 @@ function activeCursor(e) {
 function navToggle(e) {
   if (!e.target.classList.contains("active")) {
     e.target.classList.add("active");
-    gsap.to(".line1", 0.5, { rotate: "45", y: 5, background: "black" });
-    gsap.to(".line2", 0.5, { rotate: "-45", y: -5, background: "black" });
-    gsap.to("#logo", 1, { color: "black" });
+    gsap.to(".line1", 0.5, { rotate: "45", y: 5, background: "white" });
+    gsap.to(".line2", 0.5, { rotate: "-45", y: -5, background: "white" });
+    gsap.to("#logo", 1, { color: "white" });
     gsap.to(".nav-bar", 1, { clipPath: "circle(2500px at 100% -10%)" });
     document.body.classList.add("hide");
   } else {
